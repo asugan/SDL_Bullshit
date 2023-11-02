6ca9343d9b8bb8af4967fea094eb8a84c8df35d4
Çağatay Erkin Eren <67156394+asugan@users.noreply.github.com>
2023-11-02 18:57:17 +0300

    new class

---
+++ src/core/Engine.cpp
@@ -0,0 +1,49 @@
+#include "Engine.h"
+#include <iostream>
+
+Engine *Engine::s_Instance = nullptr;
+
+bool Engine::Init()
+{
+  if (SDL_Init(SDL_INIT_VIDEO) != 0 && IMG_Init(IMG_INIT_JPG | IMG_INIT_PNG) != 0)
+  {
+    SDL_Log("Failed : %s", SDL_GetError());
+    return false;
+  }
+
+  m_Window = SDL_CreateWindow("Soft Engine", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCREEN_HEIGHT, SCREEN_WIDTH, 0);
+
+  if (m_Window == nullptr)
+  {
+    SDL_Log("Failed : %s", SDL_GetError());
+    return false;
+  }
+
+  m_Renderer = SDL_CreateRenderer(m_Window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+
+  if (m_Renderer == nullptr)
+  {
+    SDL_Log("Failed TO CREATE RENDERER : %s", SDL_GetError());
+    return false;
+  }
+
+  return m_IsRunning = true;
+}
+
+void Engine::Update()
+{
+}
+
+void Engine::Events()
+{
+}
+
+void Engine::Render()
+{
+  SDL_SetRenderDrawColor(m_Renderer, 124, 218, 254, 255);
+  SDL_RenderPresent(m_Renderer);
+}
+
+bool Engine::Clean()
+{
+}
